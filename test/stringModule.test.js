a2d0ec652e1e2649252076a7a559ec1c47c7862b
pllq <gleb.brodskiy.04@gmail.com>
2025-03-23 18:29:18 +0200

    Fix chai import to ensure CommonJS compatibility

--- test/stringModule.test.js
+++ test/stringModule.test.js
@@ -1,4 +1,6 @@
-import { expect } from 'chai';
+const chai = require('chai');
+const expect = chai.expect;
+
 const { reverseString, isPalindrome } = require('../stringModule');
 
 describe('String Module Tests', () => {
