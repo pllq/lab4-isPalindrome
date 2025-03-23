a3ccbab1c7a4a8e0198a74832575ff0c31fdca1f
pllq <gleb.brodskiy.04@gmail.com>
2025-03-23 18:21:43 +0200

    Force chai to be treated as CommonJS module

--- test/stringModule.test.js
+++ test/stringModule.test.js
@@ -1,4 +1,4 @@
-const { expect } = require('chai');
+const { expect } = require('chai/lib/chai.js');
 const { reverseString, isPalindrome } = require('../stringModule');
 
 describe('String Module Tests', () => {
